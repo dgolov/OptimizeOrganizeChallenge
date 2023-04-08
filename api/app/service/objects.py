0b6184d6b521015b0d3926ae7e34b7156d7d95b1
dgolov <dmitriy.evgenyevich@gmail.com>
2023-04-08 14:51:31 +0300

    Added get object list entrypoint

--- api/app/service/objects.py
+++ api/app/service/objects.py
@@ -4,6 +4,10 @@ from app import schemas
 from core.repository_entity import ObjectEntity
 
 
+async def get_objects_list(session) -> Any:
+    print(123)
+    return await ObjectEntity(session).get_objects_list()
+
+
 async def create_object(object_: schemas.object.CreateObject, session) -> Any:
-    object_entity = ObjectEntity(session)
-    return await object_entity.create(data=object_)
+    return await ObjectEntity(session).create(data=object_)
