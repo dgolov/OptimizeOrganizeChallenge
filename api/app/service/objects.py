09017d29401dcc1bfb0e564631187de27b11d49a
dgolov <dmitriy.evgenyevich@gmail.com>
2023-04-08 17:22:27 +0300

    added crud methods for tasks and solutions

--- api/app/service/objects.py
+++ api/app/service/objects.py
@@ -1,8 +1,10 @@
 from typing import Any
 
 from app import schemas
-from core.repository_entity import ObjectEntity
+from core.repository_entity import ObjectEntity, TaskEntity, SolutionEntity
 
+
+# Objects endpoints
 
 async def get_objects_list(session, skip: int, limit: int) -> Any:
     return await ObjectEntity(session).get_objects_list(skip, limit)
@@ -24,3 +26,38 @@ async def create_object_from_xml(xml_str: str, session) -> Any:
     object_ = schemas.object.CreateObject.from_xml(xml_str)
     return await ObjectEntity(session).create(data=object_)
 
+
+# Tasks endpoints
+
+async def get_task_list(session) -> Any:
+    return await TaskEntity(session).get_task_list()
+
+
+async def create_task(object_: schemas.task.CreateTask, session) -> Any:
+    return await TaskEntity(session).create(data=object_)
+
+
+async def update_task(pk: int, object_: schemas.task.CreateTask, session) -> Any:
+    return await TaskEntity(session).update(pk=pk, data=object_)
+
+
+async def delete_task(pk: int, session) -> Any:
+    return await TaskEntity(session).delete(pk=pk)
+
+
+# Solutions endpoints
+
+async def get_solution_list(session) -> Any:
+    return await SolutionEntity(session).get_solution_list()
+
+
+async def create_solution(object_: schemas.solution.CreateSolution, session) -> Any:
+    return await SolutionEntity(session).create(data=object_)
+
+
+async def update_solution(pk: int, object_: schemas.solution.CreateSolution, session) -> Any:
+    return await SolutionEntity(session).update(pk=pk, data=object_)
+
+
+async def delete_solution(pk: int, session) -> Any:
+    return await SolutionEntity(session).delete(pk=pk)
--- api/app/views/objects.py
+++ api/app/views/objects.py
@@ -7,6 +7,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 router = APIRouter()
 
 
+# Objects routes
+
 @router.get("/")
 async def get_object(
         skip: int = schemas.basic.PAGINATION_SKIP,
@@ -46,3 +48,67 @@ async def upload_object(file: UploadFile,
                         session: AsyncSession = Depends(get_async_session),):
     contents = await file.read()
     return await service.create_object_from_xml(contents.decode(), session)
+
+
+# Tasks routes
+
+@router.get("/tasks")
+async def get_object(session: AsyncSession = Depends(get_async_session)):
+    return await service.get_task_list(session)
+
+
+@router.post("/tasks")
+async def create_object(
+        object_: schemas.task.CreateTask,
+        session: AsyncSession = Depends(get_async_session),
+):
+    return await service.create_task(object_, session)
+
+
+@router.put("/tasks")
+async def update_object(
+        pk: int,
+        object_: schemas.task.CreateTask,
+        session: AsyncSession = Depends(get_async_session),
+):
+    return await service.update_task(pk, object_, session)
+
+
+@router.delete("/tasks")
+async def delete_object(
+        pk: int,
+        session: AsyncSession = Depends(get_async_session),
+):
+    return await service.delete_task(pk, session)
+
+
+# Solutions routes
+
+@router.get("/solutions")
+async def get_object(session: AsyncSession = Depends(get_async_session)):
+    return await service.get_solution_list(session)
+
+
+@router.post("/solutions")
+async def create_object(
+        object_: schemas.solution.CreateSolution,
+        session: AsyncSession = Depends(get_async_session),
+):
+    return await service.create_solution(object_, session)
+
+
+@router.put("/solutions")
+async def update_object(
+        pk: int,
+        object_: schemas.solution.CreateSolution,
+        session: AsyncSession = Depends(get_async_session),
+):
+    return await service.update_solution(pk, object_, session)
+
+
+@router.delete("/solutions")
+async def delete_object(
+        pk: int,
+        session: AsyncSession = Depends(get_async_session),
+):
+    return await service.delete_solution(pk, session)
